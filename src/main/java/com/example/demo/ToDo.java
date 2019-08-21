a86a4851abca8bd07253cb3d04b68bfbf142f27f
NoraYu <yxy_1990@hotmail.com>
2019-08-21 12:41:48 -0400

    1

---
+++ src/main/java/com/example/demo/ToDo.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String taskname;
+
+    @NotNull
+    private int duedate;
+
+    @NotNull
+    private String priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskname() {
+        return taskname;
+    }
+
+    public void setTaskname(String taskname) {
+        this.taskname = taskname;
+    }
+
+    public int getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(int duedate) {
+        this.duedate = duedate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
